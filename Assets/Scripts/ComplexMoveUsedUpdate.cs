f9ef7b6ca0d064f9c4cb284574454fa7b2e832ba
Masataka Kohagura <masalinkinpark@gmail.com>
2016-07-17 01:17:09 +0900

    add some sctipts

---
+++ Assets/Scripts/ComplexMoveUsedUpdate.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComplexMoveUsedUpdate : MonoBehaviour
+{
+    private enum MoveState
+    {
+        Right,
+        Up,
+        LeftAndRotate,
+        End,
+    }
+
+    [SerializeField] private GameObject _cube;
+
+    private MoveState _state;
+
+    private Vector3 _movedRightEndPosition = new Vector3(5, 0, 0);
+
+    private Vector3 _movedUpEndPosition = new Vector3(5, 5, 0);
+
+    private Vector3 _movedLeftEndPosition = new Vector3(0, 5, 0);
+
+    private void Start()
+    {
+        _cube.transform.position = new Vector3(0, 0, 0);
+        _state = MoveState.Right;
+    }
+
+    private void Update()
+    {
+        switch (_state)
+        {
+            case MoveState.Right:
+                _cube.transform.position = Vector3.MoveTowards(_cube.transform.position, _movedRightEndPosition, 0.1f);
+                if (_cube.transform.position == _movedRightEndPosition)
+                {
+                    _state = MoveState.Up;
+                }
+                break;
+            case MoveState.Up:
+                _cube.transform.position = Vector3.MoveTowards(_cube.transform.position, _movedUpEndPosition, 0.1f);
+                if (_cube.transform.position == _movedUpEndPosition)
+                {
+                    _state = MoveState.LeftAndRotate;
+                }
+                break;
+            case MoveState.LeftAndRotate:
+                _cube.transform.position = Vector3.MoveTowards(_cube.transform.position, _movedLeftEndPosition, 0.1f);
+                if (_cube.transform.position == _movedLeftEndPosition)
+                {
+                    _state = MoveState.End;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/SimpleMoveUsedUpdate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Update で GameObject を動かしてみる
+/// </summary>
+public class SimpleMoveUsedUpdate : MonoBehaviour
+{
+    private enum MoveState
+    {
+        Right,
+        Up,
+        Left,
+    }
+
+    [SerializeField] private GameObject _cube;
+
+    private void Start()
+    {
+        _cube.transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        _cube.transform.position = Vector3.MoveTowards(_cube.transform.position, new Vector3(5, 0, 0), 0.1f);
+    }
+}
