841523cf71f3ebe5e01dd359a2c8782c5a742843
Masataka Kohagura <masalinkinpark@gmail.com>
2016-07-17 23:34:55 +0900

    たくさんのいろんなものを追加

---
+++ Assets/Example/BasicDOTween/MoveX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class MoveX : MonoBehaviour
+{
+    private void Start()
+    {
+        DOTween.Init();
+        DOTween.Sequence().Append(this.transform.DOMoveX(3, 1));
+    }
+}
---
+++ Assets/Example/OnComplete/OnComplete.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class OnComplete : MonoBehaviour
+{
+    [SerializeField] private GameObject _finishTextObj;
+
+    private void Start()
+    {
+        DOTween.Init();
+        DOTween.Sequence()
+            .Append(this.transform.DOMove(new Vector3(5, 0, 0), 1))
+            .OnComplete(ActiveText);
+    }
+
+    private void ActiveText()
+    {
+        _finishTextObj.SetActive(true);
+    }
+}
